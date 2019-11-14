91042940ed2ef70e9d331be28b34bea03359eb41
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2019-11-13 23:00:51 -0700

    added-a-welcome-page

---
+++ better-professor/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+const Welcome = () => {
+
+	return (
+		<div className="welcome">
+            <h1>Welcome To Bettor Professor®</h1>
+            <h2>Please Log in or register!</h2>
+		</div>
+	);
+};
+
+export default Welcome;
