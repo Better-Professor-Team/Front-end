1630e2db6078d1e99012589335e80324e32732c2
Jacob Williams <JaWilliamsDev@gmail.com>
2019-11-15 14:05:35 -0500

    added more actions

--- better-professor/src/components/Actions/actions.js
+++ better-professor/src/components/Actions/actions.js
@@ -4,8 +4,17 @@ export const GET_STUDENTS = "GET_STUDENTS";
 export const SEE_STUDENTS = "SEE_STUDENTS";
 export const ERR_STUDENTS = "ERR_STUDENTS";
 export const ADD_STUDENTS = "ADD_STUDENTS";
+export const DEL_STUDENTS = "DEL_STUDENTS";
 
-export const fetchStudents = () => dispatch => {
+export const ADD_MESSAGES = "ADD_MESSAGES";
+export const GET_MESSAGES = "GET_MESSAGES";
+export const DEL_MESSAGES = "DEL_MESSAGES";
+
+export const ADD_PROJECTS = "ADD_PROJECTS";
+export const GET_PROJECTS = "GET_PROJECTS";
+export const DEL_PROJECTS = "DEL_PROJECTS";
+
+export const getStudents = () => dispatch => {
   dispatch({ type: GET_STUDENTS });
   axios
     .get()
@@ -16,6 +25,57 @@ export const fetchStudents = () => dispatch => {
 export const addStudents = student => dispatch => {
   axios
     .post()
+    .then()
+    .catch();
+};
+
+export const delStudents = student => dispatch => {
+  axios
+    .delete()
+    .then()
+    .catch();
+};
+
+export const getMessages = () => dispatch => {
+  dispatch({ type: GET_MESSAGES });
+  axios
+    .get()
+    .then()
+    .catch();
+};
+
+export const addMessages = message => dispatch => {
+  axios
+    .post()
+    .then()
+    .catch();
+};
+
+export const delMessage = message => dispatch => {
+  axios
+    .delete()
+    .then()
+    .catch();
+};
+
+export const getProjects = () => dispatch => {
+  dispatch({ type: GET_PROJECTS });
+  axios
+    .get()
+    .then()
+    .catch();
+};
+
+export const addProjects = project => dispatch => {
+  axios
+    .put()
+    .then()
+    .catch();
+};
+
+export const delProjects = project => dispatch => {
+  axios
+    .delete()
     .then()
     .catch();
 };
